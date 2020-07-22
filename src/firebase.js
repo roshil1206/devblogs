e139425a8bb4574c6d0e27cdc2e259b4c6141c95
Roshil Kapatel <roshil.170410116026@gmail.com>
2020-07-23 00:36:46 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase';
 
 
   const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyA8PR4XRdnMRIzZv9oV6BCujlI5PPZM7Jg",
-    authDomain: "blogs-ad3f9.firebaseapp.com",
-    databaseURL: "https://blogs-ad3f9.firebaseio.com",
-    projectId: "blogs-ad3f9",
-    storageBucket: "blogs-ad3f9.appspot.com",
-    messagingSenderId: "708371859796",
-    appId: "1:708371859796:web:827a913f8937c873fe60c1",
-    measurementId: "G-CPJ0N67LYT"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   })
   const db = firebaseApp.firestore()
 
